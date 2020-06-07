85756c5692251a89c6b479c7318dfbef506d2b9a
Jose Cabral <cabralcochi@gmail.com>
2020-06-07 10:05:42 +0200

    add cahngecolor at backend

--- web/Backend.py
+++ web/Backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import _thread
+from rpi_ws281x import Color
 
 class Backend:
     app = Flask(__name__)
@@ -21,3 +22,11 @@ def show_post(row_pos):
     Backend.app.matrix.turn_rows_on([row_pos])
     Backend.app.matrix.refresh()
     return "Ok"
+
+@Backend.app.route('/turn_on/<int:row_pos>/<int:column_pos>/<color>', methods=['POST'])
+def turn_on(row_pos,column_pos, color):
+    rgb_color = [int(color[i:i+2], 16) for i in (1, 3, 5)]
+    Backend.app.matrix.turn_on_matrix_position([(row_pos, column_pos)], Color(rgb_color[0], rgb_color[1], rgb_color[2]))
+    Backend.app.matrix.refresh()
+    return "Ok"
+
--- web/static/javascript.js
+++ web/static/javascript.js
@@ -7,14 +7,39 @@ function createMatrix(letters) {
     var tr = document.createElement('tr');
     for (var column = 0; column < letters[row].length; column++) {
         var td = document.createElement('td');
-		tr.appendChild(td)
-		td.innerHTML = letters[row][column] 
+        tr.appendChild(td)
+        
+        var div = document.createElement('div');
+		td.appendChild(div)
+		div.id = row + " " + column
+		div.setAttribute('is_on', '0')
+		div.onclick = function(){
+			var rowAndColumn = this.id.split(" ")
+			var color = null
+			if (this.getAttribute('is_on') == '0'){
+				var colorElement = document.getElementById("matrixColor")
+				color = colorElement.value	
+				this.setAttribute('is_on', '1')
+			} else {
+				color = '#000000'
+				this.setAttribute('is_on', '0')
+			}
+			this.style.background =  color
+			changeColor(rowAndColumn[0], rowAndColumn[1], color)
+		}
+		div.innerHTML = letters[row][column] 
     }
     table.appendChild(tr);
   }
-  matrixArea.appendChild(table)
+  var colorElement = document.getElementById("matrixColor")
+  matrixArea.insertBefore(table, colorElement)
 }
 
+
+function changeColor(row, column, color){
+	xhttp.open("POST", "/turn_on/" + row + "/" + column "/" + color, true);
+	xhttp.send();
+}
 
 function setSameHeight(elementId) {
 	var element = document.getElementById(elementId);
